069bfcbc4b512316322cbbe0aec488d25e0dfe7b
Jamaal Primus <jamaal.primus@devopsgroup.com>
2022-07-16 10:02:56 +0100

    Update rules.mk

--- keyboards/keychron/k2/rules.mk
+++ keyboards/keychron/k2/rules.mk
@@ -41,12 +41,12 @@ OPT_DEFS = -O2
 #
 LTO_ENABLE = no
 BACKLIGHT_ENABLE = no
-MAGIC_ENABLE = yes
-MAGIC_KEYCODE_ENABLE = yes
+MAGIC_ENABLE = no
+MAGIC_KEYCODE_ENABLE = no
 BOOTMAGIC_ENABLE = full # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no    # Mouse keys
-EXTRAKEY_ENABLE = yes    # Audio control and System control
-CONSOLE_ENABLE = yes# Console for debug
+EXTRAKEY_ENABLE = yes   # Audio control and System control
+CONSOLE_ENABLE = yes    # Console for debug
 COMMAND_ENABLE = no     # Commands for debug and configuration
 SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no        # USB Nkey Rollover
